65fb47df2d9412bdf93214b007625ffe9e99409b
eddt <etwilbeck@marketingincolor.com>
2016-10-24 09:05:00 -0500

    inital add of landing pages

---
+++ you/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta charset="utf-8">
+        <title>Our World Revolves Around You</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <style>
+            /* Foundation Overrides */
+            .row {
+                margin: 0 auto;
+                max-width: 60em; /*changes 62.5em (1000px) to 60mem (960px)*/
+                width: 100%;
+            }
+            /* Page Components */
+            
+        </style>
+    </head>
+    <body>
+        <div class="row">
+            <div class="small-12 columns">
+                You Landing Page Header
+            </div>
+        </div>
+        <div class="row">
+            <div class="small-12 columns">
+                You Landing Page Content
+            </div>
+        </div>
+        <div class="row">
+            <div class="small-12 columns">
+                You Landing Page Footer
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>
