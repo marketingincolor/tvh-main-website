de82e1caa5b478a17c73798139e4729e61929af4
eddt <etwilbeck@marketingincolor.com>
2017-03-17 08:23:29 -0500

    new landing pages

---
+++ primarycare/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include( '../gtm_head.php' ); ?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta charset="utf-8">
+        <title>Our World Revolves Around You</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css" />
+        <link href="../tvh-lander.css" rel="stylesheet" type="text/css" />
+        <script type='text/javascript'>
+        var w = 520;
+        var h = 480;
+        var left = Math.round((screen.width/2)-(w/2));
+        var tops = Math.round((screen.height/2)-(h/2));
+        function showPopup(url) {
+            newwindow=window.open(url,'name','height=480,width=520,left='+left+'top='+tops+',resizable');
+            if (window.focus) {newwindow.focus()}
+        }
+        function copyToClipboard(element) {
+            var $temp = $("<input>");
+            $("body").append($temp);
+            $temp.val($(element).text()).select();
+            document.execCommand("copy");
+            $temp.remove();
+        }
+        </script>
+
+        <style>
+        .video {
+            position: relative;
+            padding-bottom: 56.25%; /* 16:9 */
+            padding-top: 25px;
+            height: 0;
+        }
+        .video iframe {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }
+        </style>
+    </head>
+    <body>
+        <?php include( '../gtm_body.php' ); ?>
+        <div class="row">
+            <div class="logo small-12 small-centered columns align-self-middle">
+                <div class="row collapse">
+                    <div class="small-12 small-centered columns">
+                        <img src="../images/tvhlp-grfx-logo.png" alt="The Villages Health">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="collapse row">
+            <div class="header small-12 small-centered columns">
+                <img src="../images/tvhlp-grfx-pricare-img-hdr.jpg" alt="The Villages Health">
+            </div>
+        </div>
+        <div class="row">
+            <div class="h1-top small-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h1 class="orange">The Importance of a Primary Care Physician</h1>
+                        <p class="main">Remember the popular television show "Marcus Welby, M.D." in the 1970s? Dr. Welby was on a first-name basis with his patients and had an unhurried, friendly bedside manner. This is the kind of close relationship we encourage our doctors to have with their patients at The Villages Health<sup>&reg;</sup>(TVH), allotting a full hour for first-time appointments and 30 minutes for regular appointments. But this is far from the norm today, as this <a href="https://www.forbes.com/sites/brucelee/2016/09/10/time-to-change-the-15-minute-limit-for-doctor-visits/#4c1c9abd3477" target="_blank" class="orange"><i>Forbes</i> article explains</a>. It asks (and answers) the question: "So how exactly did 15 minutes become the typical doctor's visit length, and why isn't this number being questioned more?"</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="small-10 large-12 small-centered columns"></div>
+        </div>
+
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2>The Problem with the Fee-for-Service Payment Model</h2>
+                        <p class="main">Typical primary care physicians only get paid for each patient they see, so there is a tendency to see as many patients as possible to meet the costs of their practice. This model doesn’t adequately support primary care, explains surgeon Atul Gawande in <a href="http://www.newyorker.com/magazine/2017/01/23/the-heroism-of-incremental-care" target="_blank" class="orange">this <i>New Yorker</i> article</a>: "We devote vast resources to intensive, one-off procedures, while starving the kind of steady, intimate care that often helps people more." TVH's revolutionary model has been structured to make primary care a priority. Our doctors are paid a salary and have a set limit of patients they see, which is half the number that typical doctors see.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="h2-know large-12 small-centered columns">
+                <div class="row collapse" data-equalizer>
+                    <div class="small-12 columns" data-equalizer-watch>
+                        <div class="row collapse">
+                            <div class="circle-wrapper-blue">
+                                <div class="circle-container-blue">
+                                    <br />
+                                    <h2>Countless studies show that better relationships between PCPs and their patients lead to better health outcomes.</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2>Better Primary Care Leads to Better Health Outcomes</h2>
+                        <p class="main">Many studies illustrate that a focus on primary care and better relationships between primary care physicians and their patients lead to better health outcomes, such as <a href="http://content.healthaffairs.org/content/34/7/1113.abstract?sid=87e6c681-e9f9-43fc-9e29-97a4c9ec2f05" target="_blank" class="orange">this study</a> that showed Californians who had more primary care had fewer ER visits and hospitalizations. This makes sense when you think about it, because one of the most important benefits of primary care is a focus on preventive care. At TVH, your close relationship with your doctor allows you to work closely with him or her to set and reach your personal health goals, which in many cases means you're stopping health problems before they start.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2>"A Century Out of Date"</h2>
+                        <p class="main">According to <a href="http://buck.exstream.tv/dev/our_faculty/bredesenlab/" target="_blank" class="orange">Dr. Dale Bredesen</a>, an original founder of the Buck Institute for Research on Aging, American health care "is a century out of date – designed to respond to acute illnesses, such as pneumonia or trauma, when the vast majority of Americans are now dying of chronic illnesses, such as cardiovascular disease and cancer.” The ability for you to work with your TVH doctor on wellness and disease prevention programs is critical to your health. And this shift to preventive care promises a dramatic impact, because as Dr. Bredesen explains, “mountains of data have now been amassed showing that these same wellness approaches go a long way toward prevention of dementia, cardiovascular disease, cerebrovascular disease, osteoporosis, cancer, and other age-associated chronic illnesses."</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2>TVH's Revolutionary Model</h2>
+                        <p class="main">While other practices struggle to improve their outdated business models, TVH benefits from being only five years young and creating a health system from the ground up that provides primary care as it should be: all revolving around your specific needs, all coordinated by your primary care doctor, and all conveniently located right here in our community, just a short golf car ride away.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="h2-content small-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-12 medium-6 medium-offset-6 columns">
+                        <p class="main">
+                            <a href="../you"><span style="text-decoration:underline;">Learn more about The Villages Health</span> &nbsp;&gt;</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row footer orangebg">
+            <div class="small-12 columns" style="color:#fff;">
+                &copy; <?php echo date("Y") ?> The Villages Health<sup>&reg;</sup>
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/foundation/6.1.2/js/foundation.equalizer.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>
