cc60c20c620ec49a10a1e08990a16d3c7091c4d2
eddt <etwilbeck@marketingincolor.com>
2017-05-10 09:06:42 -0500

    new audiology lander added

--- hearing-loss-silences-more-than-sound/index.php
+++ hearing-loss-silences-more-than-sound/index.php
@@ -10,46 +10,13 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css" />
         <link href="../tvh-lander.css" rel="stylesheet" type="text/css" />
-        <script type='text/javascript'>
-        var w = 520;
-        var h = 480;
-        var left = Math.round((screen.width/2)-(w/2));
-        var tops = Math.round((screen.height/2)-(h/2));
-        function showPopup(url) {
-            newwindow=window.open(url,'name','height=480,width=520,left='+left+'top='+tops+',resizable');
-            if (window.focus) {newwindow.focus()}
-        }
-        function copyToClipboard(element) {
-            var $temp = $("<input>");
-            $("body").append($temp);
-            $temp.val($(element).text()).select();
-            document.execCommand("copy");
-            $temp.remove();
-        }
 
-        </script>
-
-        <style>
-        .video {
-        	position: relative;
-        	padding-bottom: 56.25%; /* 16:9 */
-        	padding-top: 25px;
-        	height: 0;
-        }
-        .video iframe {
-        	position: absolute;
-        	top: 0;
-        	left: 0;
-        	width: 100%;
-        	height: 100%;
-        }
-        </style>
     </head>
     <body class="info">
         <?php include( '../gtm_body.php' ); ?>
-        <div class="row">
-            <div class="logo small-12 small-centered columns align-self-middle">
-                <div class="row collapse">
+        <div class="expanded row">
+            <div class="logo orangebg small-12 small-centered columns align-self-middle">
+                <div class="row">
                     <div class="small-12 small-centered columns">
                         <img src="../images/tvhlp-grfx-logo.png" alt="The Villages Health">
                     </div>
@@ -57,8 +24,8 @@
             </div>
         </div>
         <div class="collapse row">
-            <div class="video small-12 small-centered columns">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/DCi3HQgzqi4" frameborder="0" allowfullscreen></iframe>
+            <div class="header h1-top small-12 small-centered columns">
+                <img src="../images/tvhlp-grfx-aud-img-hdr.png" alt="Image of friends in conversation">
             </div>
         </div>
         <div class="row">
@@ -66,133 +33,69 @@
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
                         <h1 class="orange">Hearing Loss Silences More than Sound</h1>
-                        <p class="main">If you have trouble hearing, every aspect of your life is impacted.
-Conversations can be frustrating. Watching your favorite shows or listening to music can be stressful. Phone calls are often difficult. You may be embarrassed about constantly asking people to repeat what they’ve said. You may start choosing to skip social situations and spend more time alone. If you can relate, you are not alone. According to the <a href="https://www.nidcd.nih.gov/health/statistics/quick-statistics-hearing" target="_blank">National Institutes of Health</a>, nearly 25 percent of people 65 to 74, and 50 percent of those 75 and older, have disabling hearing loss. The Villages Health<sup>&reg;</sup> (TVH) multi-specialty network of providers includes a team of board-certified doctors of audiology who offer the help you need.</p>
+                        <p class="main">If you have trouble hearing, every aspect of your life is impacted. Conversations can be frustrating. Watching your favorite shows or listening to music can be stressful. Phone calls are often difficult. You may be embarrassed about constantly asking people to repeat what they’ve said. You may start choosing to skip social situations and spend more time alone. If you can relate, you are not alone. According to the <a href="https://www.nidcd.nih.gov/health/statistics/quick-statistics-hearing" target="_blank" class="orange underline">National Institutes of Health</a>, nearly 25 percent of people 65 to 74, and 50 percent of those 75 and older, have disabling hearing loss. The Villages Health (TVH) multi-specialty network of providers includes a team of board-certified doctors of audiology who offer the help you need.</p>
                     </div>
                 </div>
-
             </div>
             <div class="small-10 large-12 small-centered columns"></div>
         </div>
-
-        <div class="row">
-            <div class="h2-cta large-12 small-centered columns">
-                <div class="row collapse" data-equalizer>
-                    <div class="small-12 large-6 columns" data-equalizer-watch>
-                        <div class="each row collapse">
-                            <div class="circle-wrapper">
-                                <div class="circle-container">
-                                    <h2>Visit any UnitedHealthcare Medicare Store to learn more.</h2><br />
-                                    <a class="cta-button" href="http://thevillageshealth.com/info/uhc-medicare-stores">Locations</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="small-12 large-6 columns" data-equalizer-watch>
-                        <div class="each row collapse">
-                            <div class="circle-wrapper">
-                                <div class="circle-container">
-                                    <h2><br />Visit any Medicare Resource Center to learn more.</h2><br />
-                                    <a class="cta-button" href="http://thevillageshealth.com/info/medicare-resource-centers">Locations</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="row">
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2 class="orange">Understanding the Clinical Value of Medicare Advantage</h2>
-                        <p class="main">Learn how Medicare Advantage is helping to transform health care for the better, resulting in better health outcomes for patients. <a href="../study">Click here to learn more and watch video</a>.</p>
+                        <h2>Doctors of Audiology Can Turn Up the Volume...of Your Life</h2>
+                        <p class="main">As TVH Director of Audiology, Al Turri, AuD, is concerned about the many people who go to "hearing aid specialists" at retail stores and mistakenly think they're seeing doctors, when they're actually being treated by someone trained only to give hearing tests and sell hearing aids. "That's why at The Villages Health, we only hire doctors of audiology," he explains. With a highly educated knowledge of anatomy and physiology, ear disorders and brain health, our audiology team has a medical perspective not only on hearing, but also on preserving speech perception and understanding. We use Real Ear Measurements (REM), which allow us to measure the output of the hearing aid at the eardrum and analyze whether or not it's providing the needed gain for you to thrive. It's a huge game changer that has enabled us to help our patients hear better and be satisfied with their hearing aids. Our team can provide you with a hearing aid that meets your needs and fits your lifestyle. And, if you already have hearing aids, we can do this REM analysis to see if they're working correctly.</p>
                     </div>
                 </div>
-                <hr>
             </div>
         </div>
         <div class="row">
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2 class="orange">Expanding Our Circle of Specialty Care</h2>
-                        <p class="main">TVH continues to grow our specialty care offerings and the services available to you. We've expanded our behavioral health program to offer treatment for every aspect of your health, from physical to emotional to psychological. We invite you to learn more about these behavioral health services available to you as a patient receiving primary care through TVH. <a href="./behavioral-health">Click here to learn more</a>.</p>
-                        <p class="main">We have added 24 providers in 2016 to ensure all of our patients have convenient access to exceptional care whenever it's needed.</p>
+                        <h2>The Perfect Model for Hearing Health</h2>
+                        <p class="main">All of our physicians partner with the audiology team to provide a deeper level of auditory related care. Because prevention and early treatment are key to hearing health, Dr. Turri is an experienced advocate of putting audiology directly in a primary care setting. He calls it "the perfect model," with three unique benefits: hearing screening and diagnoses happen routinely and generally at an earlier age, initial and ongoing treatment is quickly and conveniently available, and high quality hearing aids are more affordable because our doctors are not motivated by commissions and we have no financial ties to hearing aid manufacturers as do most retailers. It's a win-win-win for patients. Anyone in the community can utilize TVH audiology services.</p>
                     </div>
                 </div>
-                <hr>
             </div>
         </div>
         <div class="row">
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2 class="orange">Hospital Care for The Villages Health Patients with Medicare Advantage</h2>
-                        <p class="main">Are you aware that a TVH Hospitalist and Nurse Navigator care for TVH patients with Medicare Advantage if they become hospitalized at The Villages<sup>&reg;</sup> Regional Hospital? We invite you to learn more about our hospital medicine program.  <a href="./hospital-care">Click here to learn more</a>.</p>
+                        <h2>What You Don't Hear Can Hurt You</h2>
+                        <p class="main">Unaddressed hearing loss can have long-term, potentially devastating effects beyond lifestyle issues. <a href="http://www.hopkinsmedicine.org/news/media/releases/hearing_loss_and_dementia_linked_in_study" target="_blank" class="orange underline">A joint study by John Hopkins Medicine and the National Institute on Aging</a> has released powerful data showing untreated hearing loss results in a higher incidence of early onset dementia. </p>
+                    
+                        <p class="main">Dr. Turri says those findings further support the need for early detection and correction. "When patients delay treatment, brain function and cognitive ability can be impaired significantly from auditory deprivation."  To that end, Dr. Turri has implemented educational programs on the subject, along with a brain fitness program for patients.</p>
                     </div>
                 </div>
-                <hr>
             </div>
         </div>
         <div class="row">
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2 class="orange">Hospitals Accepting Medicare Advantage</h2>
-                        <p class="main">If you choose to leave our community for care, we invite you to learn more about the hospitals that accept Medicare Advantage. <a href="./hospital-coverage">Click here to learn more</a>.</p>
+                        <h2>"I love what I do so much. Being able to help people hear again is very rewarding."</h2>
+                        <p class="main">As with all TVH medical professionals, Dr. Turri's passion for his patients' well-being is evidenced by a commitment to working together as a team in providing the highest levels of primary and specialty care.</p>
                     </div>
                 </div>
-                <hr>
-            </div>
-        </div>
-        <div class="row">
-            <div class="h2-content large-12 small-centered columns">
-                <br />
-                <div class="collapse row">
-                    <div class="small-10 small-offset-1 columns">
-                        <p class="main">It's all part of an exceptional care model built around your individual needs that links primary care, behavioral health, vision, audiology, and a growing range of specialties for highly coordinated care.</p>
-                        <p class="main">Nothing is as important as your health, so make sure you have the complete facts.</p>
-                    </div>
-                </div>
-                <br />
             </div>
         </div>
 
         <div class="row">
-            <div class="h4-share small-12 columns">
-                <h4>Revolutionary Care Centered on You</h4>
-                <p>We are here for you! To learn more about The Villages Health, please call <strong>352-702-3435</strong>, email <a href="mailto:info@TheVillagesHealth.com" style="color:#fff; text-decoration:underline;">info@TheVillagesHealth.com</a>, or visit <a href="http://thevillageshealth.com" style="color:#fff; text-decoration:underline;" target="_blank">The Villages Health</a>.</p>
-                <p>&nbsp;</p>
-            </div>
-            <div class="h3-social small-12 columns">
-                <h3>Share This Page</h3>
-                <div class="row collapse">
-                    <div class="small-2 small-offset-2 column">
-                        <a href="https://www.facebook.com/sharer/sharer.php?u=http://thevillageshealth.com/you" onclick="showPopup(this.href);return(false);" target="_blank" class="share-fb">&nbsp;</a>
-                    </div>
-                    <div class="small-2 column">
-                        <a href="https://twitter.com/home?status=Our%20World%20Revolves%20Around%20You%20http://thevillageshealth.com/you" onclick="showPopup(this.href);return(false);" class="share-tw" target="_blank">&nbsp;</a>
-                    </div>
-                    <div class="small-2 column">
-                        <a href="mailto:?subject=Our%20World%20Revolves%20Around%20You&body=http://thevillageshealth.com/you" class="share-em">&nbsp;</a>
-                    </div>
-                    <div class="small-2 column end ">
-                        <p id="p1" style="display:none;">http://thevillageshealth.com/you</p>
-                        <textarea id="holdtext" style="display:none;"></textarea>
-                        <a href="javascript:;" onclick="copyToClipboard('#p1');" class="share-li">&nbsp;</a>
+            <div class="h2-content small-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-12 medium-6 medium-offset-6 columns">
+                        <p class="main">
+                            <a href="../audiology/"><span style="text-decoration:underline;">Learn more about The Villages Health</span> &nbsp;&gt;</a>
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="h4-like small-12 columns"><h4><a href="https://www.facebook.com/TheVillagesHealth" target="_blank"><img src="../images/tvhlp-grfx-social-share-fb.jpg" style="position:relative; top:-4px; padding-right:.25em;">Like Us On Facebook</a></h4>
-            </div>
-        </div>
 
-        <div class="row footer">
-            <div class="small-12 columns">
+        <div class="row footer orangebg">
+            <div class="small-12 columns" style="color:#fff;">
                 &copy; <?php echo date("Y") ?> The Villages Health
             </div>
         </div>
