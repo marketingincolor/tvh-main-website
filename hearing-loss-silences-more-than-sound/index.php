810b522f567efc8c3a7425cecd76f0185bb8b87b
eddt <etwilbeck@marketingincolor.com>
2017-05-10 09:28:01 -0500

    fixes to audio lander

--- hearing-loss-silences-more-than-sound/index.php
+++ hearing-loss-silences-more-than-sound/index.php
@@ -32,7 +32,7 @@
             <div class="h1-top small-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h1 class="orange">Hearing Loss Silences More than Sound</h1>
+                        <h1 class="orange no-pad">Hearing Loss Silences More than Sound</h1>
                         <p class="main">If you have trouble hearing, every aspect of your life is impacted. Conversations can be frustrating. Watching your favorite shows or listening to music can be stressful. Phone calls are often difficult. You may be embarrassed about constantly asking people to repeat what they’ve said. You may start choosing to skip social situations and spend more time alone. If you can relate, you are not alone. According to the <a href="https://www.nidcd.nih.gov/health/statistics/quick-statistics-hearing" target="_blank" class="orange underline">National Institutes of Health</a>, nearly 25 percent of people 65 to 74, and 50 percent of those 75 and older, have disabling hearing loss. The Villages Health (TVH) multi-specialty network of providers includes a team of board-certified doctors of audiology who offer the help you need.</p>
                     </div>
                 </div>
@@ -43,7 +43,7 @@
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2>Doctors of Audiology Can Turn Up the Volume...of Your Life</h2>
+                        <h2 class="no-pad">Doctors of Audiology Can Turn Up the Volume...of Your Life</h2>
                         <p class="main">As TVH Director of Audiology, Al Turri, AuD, is concerned about the many people who go to "hearing aid specialists" at retail stores and mistakenly think they're seeing doctors, when they're actually being treated by someone trained only to give hearing tests and sell hearing aids. "That's why at The Villages Health, we only hire doctors of audiology," he explains. With a highly educated knowledge of anatomy and physiology, ear disorders and brain health, our audiology team has a medical perspective not only on hearing, but also on preserving speech perception and understanding. We use Real Ear Measurements (REM), which allow us to measure the output of the hearing aid at the eardrum and analyze whether or not it's providing the needed gain for you to thrive. It's a huge game changer that has enabled us to help our patients hear better and be satisfied with their hearing aids. Our team can provide you with a hearing aid that meets your needs and fits your lifestyle. And, if you already have hearing aids, we can do this REM analysis to see if they're working correctly.</p>
                     </div>
                 </div>
@@ -53,7 +53,7 @@
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2>The Perfect Model for Hearing Health</h2>
+                        <h2 class="no-pad">The Perfect Model for Hearing Health</h2>
                         <p class="main">All of our physicians partner with the audiology team to provide a deeper level of auditory related care. Because prevention and early treatment are key to hearing health, Dr. Turri is an experienced advocate of putting audiology directly in a primary care setting. He calls it "the perfect model," with three unique benefits: hearing screening and diagnoses happen routinely and generally at an earlier age, initial and ongoing treatment is quickly and conveniently available, and high quality hearing aids are more affordable because our doctors are not motivated by commissions and we have no financial ties to hearing aid manufacturers as do most retailers. It's a win-win-win for patients. Anyone in the community can utilize TVH audiology services.</p>
                     </div>
                 </div>
@@ -63,7 +63,7 @@
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2>What You Don't Hear Can Hurt You</h2>
+                        <h2 class="no-pad">What You Don't Hear Can Hurt You</h2>
                         <p class="main">Unaddressed hearing loss can have long-term, potentially devastating effects beyond lifestyle issues. <a href="http://www.hopkinsmedicine.org/news/media/releases/hearing_loss_and_dementia_linked_in_study" target="_blank" class="orange underline">A joint study by John Hopkins Medicine and the National Institute on Aging</a> has released powerful data showing untreated hearing loss results in a higher incidence of early onset dementia. </p>
                     
                         <p class="main">Dr. Turri says those findings further support the need for early detection and correction. "When patients delay treatment, brain function and cognitive ability can be impaired significantly from auditory deprivation."  To that end, Dr. Turri has implemented educational programs on the subject, along with a brain fitness program for patients.</p>
@@ -75,7 +75,7 @@
             <div class="h2-content large-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h2>"I love what I do so much. Being able to help people hear again is very rewarding."</h2>
+                        <h2 class="no-pad">"I love what I do so much. Being able to help people hear again is very rewarding."</h2>
                         <p class="main">As with all TVH medical professionals, Dr. Turri's passion for his patients' well-being is evidenced by a commitment to working together as a team in providing the highest levels of primary and specialty care.</p>
                     </div>
                 </div>
