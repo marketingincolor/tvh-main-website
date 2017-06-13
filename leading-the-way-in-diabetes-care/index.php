6da58904b3752a7bfccd4d61e7a240d2f9b0f48e
eddt <etwilbeck@marketingincolor.com>
2017-06-13 11:39:22 -0500

    new diabetes landing page

---
+++ leading-the-way-in-diabetes-care/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include( '../gtm_head.php' ); ?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta charset="utf-8">
+        <title>Leading the Way in Diabetes Care</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css" />
+        <link href="../tvh-lander.css" rel="stylesheet" type="text/css" />
+
+    </head>
+    <body class="info">
+        <?php include( '../gtm_body.php' ); ?>
+        <div class="expanded row">
+            <div class="logo orangebg small-12 small-centered columns align-self-middle">
+                <div class="row">
+                    <div class="small-12 small-centered columns">
+                        <img src="../images/tvhlp-grfx-logo.png" alt="The Villages Health">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="collapse row">
+            <div class="header h1-top small-12 small-centered columns">
+                <img src="../images/tvhlp-grfx-dia-img-hdr.png" alt="Image of friends in conversation">
+            </div>
+        </div>
+        <div class="row">
+            <div class="h1-top small-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h1 class="orange no-pad">Leading the Way in Diabetes Care</h1>
+                        <p class="main alt">If you have diabetes, the better you control it, the more likely you are to enjoy the retirement you've always dreamed about. Blindness, kidney failure, leg amputations, strokes, and heart attacks are all disabling complications of poorly controlled diabetes. That's why our diabetes program is filled with helpful tools, enabling those who suffer from diabetes to fulfill their desire to enjoy their lives. Our diabetes program is the only one in the region that's recognized by the American Diabetes Association.</p>
+                        <p class="main alt">Did you know that one in three American adults, approximately 86 million Americans, have pre-diabetes? Without weight loss and moderate physical activity, 15-30 percent of individuals with pre-diabetes will develop type 2 diabetes within five years. More than 29 million Americans or approximately 9.3 percent of the population has diabetes.</p>
+                        <p class="main alt">With these statistics in mind, we focused our efforts on building a comprehensive diabetes management and education program. This program not only allows patients to get the medical care they need to control their diabetes, but also teaches them how to more effectively manage their condition.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="small-10 large-12 small-centered columns"></div>
+        </div>
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2 class="no-pad">Managing Your Diabetes</h2>
+                        <p class="main alt">Many people diagnosed with diabetes have their condition managed well by their primary care physicians. At The Villages Health (TVH), our model of care is set up to ensure each of our patient's blood sugar is being controlled through the use of medications, nutrition, and exercise. We make sure our patients are receiving routine eye exams and tests to monitor kidney function. Coordinating the right care for a person with a chronic disease such as diabetes can minimize complications and greatly impact quality of life.</p>
+                        <p class="main alt">Our diabetes program incorporates both clinical care and education together. All of our patients have the opportunity to participate in programs that teach them about nutrition, exercise, and lifestyle changes that can keep them healthier.</p>
+                        <p class="main alt">Dr. James Mersey, TVH Endocrinologist, explains, "When a person has diabetes, there may be other medical conditions that increase the chance of complications and impact how well blood sugar levels are being controlled. As a multispecialty group, patients can be referred to a TVH endocrinologist to deliver more specialized care if needed. Our endocrinologists can diagnose other conditions, provide treatment options for complicated cases, and coordinate care with other specialists as needed."</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2 class="no-pad">A Team of Experts</h2>
+                        <p class="main alt">If you're diagnosed with diabetes, at TVH an entire team of experts all work together to help you manage your diabetes, including your primary care physician, endocrinologists, certified diabetes educators, and a registered dietician.</p>
+                        <p class="main alt">We now have two endocrinologists, one nurse practitioner, a nutritionist and three certified diabetes educators available to patients. This team coordinates the efforts of many additional TVH staff members and providers who work daily to assist and educate patients with diabetes or pre-diabetes.</p>
+                        <p class="main alt">We always welcome a patient’s spouse and family members to participate in care plans and educational classes. This team approach, including patient, family, primary care team, specialists, and wellness educators, is why our patients do better with controlling their diabetes.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2 class="no-pad">Diabetes Comprehensive Care Program</h2>
+                        <p class="main alt">We now offer a Diabetes Comprehensive Care Program at the TVH Specialty Care Center that allows anyone with diabetes to be referred to the program by a physician. Dr. Mersey explains, "Patients can take advantage of the expertise of our team and have a full service, one-stop experience to get their diabetic needs met. In the program, patients receive the devices and supplies to manage their diabetes, care coordination as needed, education, and access to additional resources."</p>
+                    
+                        <p class="main alt">Since monitoring blood sugar is critical, we use the latest technology to track our patients' daily glucose levels. "We personalize decisions about our patients' care based on their data and have the expertise to guide the best approach to keep them as healthy as possible," shares Dr. Mersey.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2 class="no-pad">A Nationally Recognized Model of Care Integrating Education</h2>
+                        <p class="main alt">Amy Wixted, MPH, CHES, CIC, Wellness and Education Manager at The Villages Health, recently presented at the national <i>American Diabetes Association's 32nd Annual Clinical Conference on Diabetes</i>. Amy educated conference attendees on how to integrate a diabetes prevention program into a diabetes education model.</p>
+                        <p class="main alt">TVH is one of the only health systems in the country that has been able to illustrate how creating this type of program can impact diabetes in the community.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="h2-content large-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-10 small-offset-1 columns">
+                        <h2 class="no-pad">Diabetes Education Class Offerings</h2>
+                        <p class="main alt">The Villages Health Learning Center offers a variety of classes for individuals with diabetes, family members, and people interested in preventing diabetes. This combination of courses allows for ongoing learning opportunities.</p>
+                        <p class="main alt">The Diabetes Education Program and Medical Nutrition Therapy Program are for individuals with type 1, type 2, and gestational diabetes. These are by referral only and are covered by most insurance plans. To learn more about these programs, call 352-674-1770.</p>
+                        <p class="main alt">Each month, free classes related to diabetes prevention and education are offered at each Care Center. Anyone can attend these classes. To see the upcoming classes and register, <a href="https://thevillageshealth.com/learningcenter/wellness-seminars.php" target="_blank" class="orange">click here</a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="h2-content small-12 small-centered columns">
+                <div class="collapse row">
+                    <div class="small-12 medium-6 medium-offset-6 columns">
+                        <p class="main">
+                            <a href="../"><span style="text-decoration:underline;">Learn more about The Villages Health</span> &nbsp;&gt;</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row footer orangebg">
+            <div class="small-12 columns" style="color:#fff;">
+                &copy; <?php echo date("Y") ?> The Villages Health
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/foundation/6.1.2/js/foundation.equalizer.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>
