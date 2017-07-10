044b4e70a799d528453073f33daee81d550f134d
eddt <etwilbeck@marketingincolor.com>
2017-07-10 09:56:40 -0500

    new urology page

---
+++ urology/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php //include( '../gtm_head.php' ); ?>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta charset="utf-8">
+        <title>Leading the Way in Diabetes Care</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css" />
+        <link href="../tvh-lander.css" rel="stylesheet" type="text/css" />
+        <style>
+            body { color:#333; }
+            .row {
+                margin: 0 auto;
+                max-width: 79.375em; /*changes 62.5em (1000px) to 60em (960px)*/
+                width: 100%;
+            }
+        </style>
+    </head>
+    <body class="info specialty">
+        <?php //include( '../gtm_body.php' ); ?>
+        <div class="expanded row">
+            <div class="logo bluebg small-12 small-centered columns align-self-middle">
+                <div class="row">
+                    <div class="small-12 small-centered large-6 columns">
+                        <img src="../images/tvhlp-grfx-spty-logo.png" alt="The Villages Health">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="collapse row">
+            <div class="header small-12 small-centered columns">
+                <h1 class="no-pad">Our World Revolves Around You</h1>
+            </div>
+        </div>
+        <div class="collapse expanded row">
+            <div class="header-image small-12 small-centered columns"></div>
+        </div>
+        <div class="row">
+            <div class="h1-spty small-12 small-centered columns">
+                <div class="row collapse">
+                    <div class="small-12 medium-7 columns">
+                        <h2>Exceptional urology care for incontinence, erectile dysfunction (ED), and more.</h2>
+                        <p class="spty-main">Our board-certified urology team has decades of experience in diagnosing and treating urological diseases and are up on the latest care advancements. They take a very personal approach to determine the right treatment for you, and involve you in the decision process. They'll also work closely with your primary care doctor to ensure the best possible care and results for your unique needs.</p>
+                        <p class="spty-main">Our patients are pleasantly surprised at the highly personal and efficient level of care they receive, resulting in better health outcomes, achieved more quickly, and in most cases, at a lower cost. Experience the difference of medicine practiced the way it was meant to be. Make your appointment today.</p>
+                    </div>
+                    <div class="small-12 medium-4 medium-offset-1 columns cta">
+                        <h2 class="cta-spty">To schedule an appointment, call <br>352-553-1157.</h2>
+                        <p class="">The Villages Health Specialty Care accepts Medicare, Medicare Supplemental, UnitedHealthcare Medicare Advantage, and most commercial plans. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="small-10 large-12 small-centered columns">&nbsp;</div>
+        </div>
+
+        <div class="row large-collapse">
+            <div class="h2-spty large-12 small-centered columns">
+                <div class="row dr">
+                    <div class="small-12 medium-2 columns dr-pic">
+                        <img src="../images/tvhlp-grfx-spty-ur-dr-jdt.jpg" alt="J. Derek Thompson, MD">
+                        <h2>J. Derek Thompson, MD</h2>
+                    </div>
+                    <div class="small-12 medium-10 columns">
+                        <h2 class="no-pad dr-quote">"I like the concept of spending more time with the patient – getting to know the patient on a much better basis than the usual five-minute visit – and the complete care of the patient. I like that approach."</h2>
+                        <div class="row collapse">
+                            <div class="small-12 medium-6 columns">
+                                <p class="spty-main">Hometown: Lagrange, GA<br>
+                                Undergraduate: Georgia State University<br>
+                                Graduate School: Medical College of Georgia<br>
+                                Internship/Residency: University of Florida Shands Hospital</p>
+                            </div>
+                            <div class="small-12 medium-6 columns">
+                                <p class="spty-main">Board Certified: American Board of Urology<br>
+                                Fellowships: University of Florida Shands Hospital, Fellow Pediatric and Reconstructive Urology, Gainesville, FL</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row dr">
+                    <div class="small-12 medium-2 columns dr-pic">
+                        <img src="../images/tvhlp-grfx-spty-ur-dr-rjm.jpg" alt="J. Derek Thompson, MD">
+                        <h2>Roger J. Madore, PA-C</h2>
+                    </div>
+                    <div class="small-12 medium-10 columns">
+                        <h2 class="no-pad dr-quote">"I look at the patient as a person first and then I put myself in their shoes. Because of the nature of urology, it's important that I make each patient feel comfortable."</h2>
+                        <div class="row collapse">
+                            <div class="small-12 medium-6 columns">
+                                <p class="spty-main">Hometown: Hartford, CT<br>
+                                Undergraduate: University of Central Florida, Orlando, FL</p>
+                            </div>
+                            <div class="small-12 medium-6 columns">
+                                <p class="spty-main">Graduate School: University of Florida, Gainesville, FL<br>
+                                Board Certified: National Commission on Certification of Physician Assistants</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="collapse expanded row">
+            <div class="footer-image small-12 small-centered columns h4-spty">
+                <div class="row">
+                    <div class="small-12 columns">
+                        <h4>Urology Conditions Include:</h4>
+                        <p class="conditions">Urinary incontinence &#124; Overactive bladder &#124; Urinary tract infection &#124; Difficulty urinating &#124; Sexual dysfunction &#124; Erectile dysfunction (ED) &#124; Kidney stones &#124; Calcium stones &#124; Enlarged prostate &#124; Prostate cancer &#124; Bladder cancer &#124; Testicular cancer &#124; Elevated PSA levels &#124; Blood in urine &#124; Pelvic organ prolapse &#124; Interstitial cystitis</p>
+                        <h4>Urology Treatments Include:</h4>
+                        <p class="conditions">Treatments for incontinence (male and female) &#124; Penile implants &#124; Vasectomy &#124; Kidney stones &#124; BCG treatment &#124; TURBT and BCG and various other treatments for bladder cancer &#124; GreenLight Laser surgery &#124; Interstim &#124; Circumcision &#124; Urinary sphincter replacement &#124; Inflatable penile prosthesis (IPP) &#124; Hydrocele repairs &#124; Cystocele repairs &#124; Rectocele repairs &#124; Bladder botox</p>
+                    </div>
+                    <div class="small-12 medium-6 medium-offset-6 columns">
+                        <p class="learn" style="float:right;">
+                            <a href="../you"><span style="text-decoration:underline;">Learn more about The Villages Health</span> &nbsp;&gt;</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row expanded footer bluebg">
+            <div class="small-12 columns" style="color:#fff;">
+                &copy; <?php echo date("Y") ?> The Villages Health
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/foundation/6.1.2/js/foundation.equalizer.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>
