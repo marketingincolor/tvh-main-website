0ff6518725ff419b8d3028ff5a7e9affed4a6717
eddt <etwilbeck@marketingincolor.com>
2017-07-25 12:14:10 -0500

    revised you landing page

--- you/index.php
+++ you/index.php
@@ -6,7 +6,7 @@
         <meta charset="utf-8">
         <title>Our World Revolves Around You</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css" />
         <link href="../tvh-lander.css" rel="stylesheet" type="text/css" />
@@ -29,9 +29,20 @@
         </script>
 
         <style>
+        .you { color:rgb(51, 51, 51); }
+        .you p { text-align:left; margin-bottom:1em !important; }
+        .personal-quote { font-size:1.015em !important; margin-top:.5em; text-align:center !important; }
+        .personal-id { font-size:1.015em !important; font-style:italic; text-align:center !important; font-weight:bold; }
         .logo { background-color:rgb(45, 122, 61); }
-        .h1-headline h1 { font-size:2em; padding: .5em 0em; color:rgb(45, 122, 61); }
-        .h3-social, .h4-share { background-color: rgb(238, 238, 238); }
+        .main a, .main a:hover { color:rgb(255, 129, 61); text-decoration:underline;}
+        .h1-intro h1, 
+        .h1-headline h1, 
+        .h4-share h4, 
+        .h3-social h3 { font-weight:300 !important; font-size:1.75em; padding:0em; color:rgb(0, 0, 0) !important; margin:.5em 0em 1em; }
+        .h4-like h4 { font-weight:300 !important; font-size:1.5em; padding:0em; color:rgb(0, 0, 0) !important; margin:1.25em 0em; }
+        .h1-intro .rule { margin:1.5em; text-align:center; }
+        .h1-intro .personal { margin:2em 0em; }
+        .h3-social, .h4-share { background-color: rgb(238, 238, 238); padding:2em 0em; }
         .h4-share .share-button { background-color:rgb(219, 104, 41); }
         .h4-share .share-button:hover { background-color:rgb(255, 129, 61); }
         .h4-like { background-color:rgb(45, 122, 61); }
@@ -42,7 +53,8 @@
         #player { position:absolute; top:0px; left:0px; }
         #overlay { position:absolute; z-index:1; top:0px; left: px; }
         .flex-video { margin-bottom:0 !important; padding-bottom:56.25% !important; }
-
+        .footer-msg h2  { font-weight:300 !important; font-size:1.75em; padding:0em; color:rgb(255, 255, 255) !important; margin:1em 0em; }
+        .footer-msg { background:url(../images/tvhlp-grfx-cta-img-foot-bg.jpg) no-repeat center center / cover; }
         .share-fb { background:url(../images/tvhlp-grfx-you-social-btn-fb.png) no-repeat center center / contain; }
         .share-fb:hover { background:url(../images/tvhlp-grfx-you-social-btn-fb.png) no-repeat center center / contain; }
 .share-fb:active { background:url(../images/tvhlp-grfx-social-btn-clk-fb.png) no-repeat center center / contain; }
@@ -57,9 +69,9 @@
 .share-li:active { background:url(../images/tvhlp-grfx-social-btn-clk-li.png) no-repeat center center / contain; }
         </style>
     </head>
-    <body>
+    <body class="you">
         <?php include( '../gtm_body.php' ); ?>
-        <div class="row">
+        <div class="row expanded">
             <div class="logo small-12 small-centered columns align-self-middle">
                 <div class="row collapse">
                     <div class="small-12 small-centered columns">
@@ -103,13 +115,10 @@
                 player.getPlayerState() == 1 ? player.pauseVideo() : player.playVideo();
             });
         }
-
-
         </script>
         
         <div class="row">
             <div class="small-12" style="position:relative;">
-                <div id="overlay"><img src="../images/tvhlp-grfx-you-curve.png" width="960" height="540"></div>
                 <div class="flex-video"><div id="player"></div></div>
             </div>
         </div>
@@ -118,12 +127,13 @@
             <div class="h1-intro small-12 small-centered columns">
                 <div class="collapse row">
                     <div class="small-10 small-offset-1 columns">
-                        <h1>By doing things differently, we're making a big difference in the lives of our patients.</h1>
-                        <p class="main">As a patient of The Villages Health, you'll benefit from our revolutionary approach that enables more focus on each patient and seamless, coordinated care across a breadth of specialties. As one of the only multi-specialty groups in the Tri-County area, we can diagnose and treat the most common to complex medical conditions. Our specialties include audiology, cardiology, dermatology, endocrinology, gastroenterology, general surgery, gynecology, orthopaedics, neurology, psychiatry, rheumatology, and urology.</p>
-                        <p class="main">Our unique health system enables you to have a closer relationship with your primary care doctor, who oversees every aspect of your care, including working closely with any specialists. We encourage you to take an active role in your own health care and provide unparalleled service and support. It's why we've been ranked in the top 1% of U.S. health systems.*</p>
-                        <p class="main"><a href="http://thevillageshealth.com/primarycare/" target="_blank">By thinking outside of the traditional health care box</a>, we're able to help our patients stay healthy with a preventive approach that stops problems before they start or discovers them sooner. Our unique health system also enables more efficient treatments, helping our patients get well faster. So whether you're managing a chronic condition or facing a serious health issue, our innovative approach gives you your best chance.</p>
-                        <p class="main">To learn more, please call The Villages Health<sup>&reg;</sup> at 352-702-3435, visit <a href="http://thevillageshealth.com/" target="_blank">TheVillagesHealth.com</a>, or email <a href="mailto:info@thevillageshealth.com">info@TheVillagesHealth.com</a>.</p>
-                        <p class="sub">*Source: UnitedHealthcare measured the performance of The Villages Health with The Healthcare Effectiveness Data and Information Set (HEDIS), a tool developed by the National Committee for Quality Assurance (NCQA) that's used by more than 90 percent of America's health plans to measure the level of care and service.</p>
+                        <h1 style="text-align:left; padding-top:1.5em;">See for yourself how exceptional health care can be.</h1>
+                        <p class="main">Remember your first trip to The Villages<sup>&reg;</sup>? You had to experience it first-hand to realize just how amazing this community is. We'd like you to experience The Villages Health in person, too, with a tour of one of our seven Care Centers. To schedule your personal tour, please call 352-702-3435.</p>
+                        <h1 style="text-align:left;">Exceptional care that gives you your best chance at enjoying good health – and the good life, right here in The Villages<sup>&reg;</sup>.</h1>
+                        <p class="main">Discover health care the way it was meant to be. Where your appointments are right on schedule and your doctor greets you by name, treats you as a friend, and spends focused time with you. Our primary care doctors thrive on serving a limited number of patients, so they can get to know you well. And it’s all just a golf car ride away, here in your very own hometown.</p>
+                        <p class="main">As the largest health care group in the Tri-County area with more than 60 board-certified doctors, we’re passionate about making a positive difference in your life. It's why we've been ranked in the top 1% of U.S. health systems.* Your primary care doctor will oversee every aspect of your care, including working closely with our highly qualified specialists in audiology, cardiology, dermatology, endocrinology, gastroenterology, general surgery, gynecology, orthopaedics, neurology, rheumatology, and urology. (You don't have to be a primary care patient to see our specialists.)</p>
+                        <p class="main"><a href="http://thevillageshealth.com/primarycare/" target="_blank">By thinking outside of the traditional health care box</a>, we're able to provide superior preventive care, diagnosis, and treatment. Our efficient and integrated health care system allows us to provide better health outcomes and an unprecedented care experience for our patients&mdash;often at a lower cost. So whether you’re managing chronic conditions or facing serious health issues, we're here to protect your greatest wealth: your health. </p>
+                        <p class="sub">*Source: UnitedHealthcare measured the performance of The Villages Health with The Healthcare Effectiveness Data and Information Set (HEDIS), a tool developed by the National Committee for Quality Assurance (NCQA) that’s used by more than 90 percent of America's health plans to measure the level of care and service.</p>
                     </div>
                 </div>
 
@@ -134,7 +144,7 @@
 
         <div class="row">
             <div class="h1-intro small-12 small-centered columns">
-                <div class="collapse row">
+                <div class="rule collapse row">
                     <div class="small-10 small-offset-1 columns">
                         <hr>
                     </div>
@@ -142,15 +152,19 @@
             </div>
         </div>
 
-        <div class="row">
-            <div class="h1-intro small-12 small-centered columns">
+        <div class="row h1-intro">
+            <div class="h1-intro small-10 small-centered columns" data-equalizer="outer">
 
-                <div class="each row" data-equalizer>
-                    <div class="small-12 medium-6 columns" data-equalizer-watch>
+                <div class="each row" data-equalizer="inner">
+                    <div class="personal small-12 medium-6 columns" data-equalizer-watch="outer">
                         <img src="../images/tvhlp-grfx-cta-img-lt.png" alt="find out more today">
+                        <p class="personal-quote" data-equalizer-watch="inner">"My care is proactive, comprehensive, and coordinated by my doctor. They set you on a course for the best possible care."</p>
+                        <p class="personal-id">Paul Hathcox, Patient at Creekside Care Center</p>
                     </div>
-                    <div class="small-12 medium-6 columns" data-equalizer-watch>
+                    <div class="personal small-12 medium-6 columns" data-equalizer-watch="outer">
                         <img src="../images/tvhlp-grfx-cta-img-rt.png" alt="dont delay">
+                        <p class="personal-quote" data-equalizer-watch="inner">"My physician found out what was wrong with me. She saved my life. I feel so confident with her and the medical help that I’ve received."</p>
+                        <p class="personal-id">Elise Brennan, Patient at Pinellas Care Center</p>
                     </div>
                 </div>
 
@@ -158,71 +172,10 @@
         </div>
 
         <div class="row">
-            <div class="h1-you small-12 small-centered columns">
-                <div class="collapse row">
-                    <h1>Revolutionary Care Centered on You</h1>
-
-                    <div class="collapse row">
-                        <div class="small-6 small-offset-3 medium-4 medium-offset-4 columns">
-                            <img src="../images/tvhlp-grfx-you-ico-clock.png" alt="appointment icon">
-                            <p>One-hour new patient appointments; 30-minute regular appointments</p>
-                        </div>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-timer.png" alt="wait icon">
-                        <p>No or short wait times</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-note.png" alt="plan icon">
-                        <p>Personalized wellness plans</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-noscope.png" alt="testing icon">
-                        <p>No unnecessary testing</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-flask.png" alt="labs icon">
-                        <p>On-site labs</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-sched.png" alt="appointments icon">
-                        <p>Same-day appointments</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-steth.png" alt="screenings icon">
-                        <p>Free screenings</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-heart.png" alt="health classes icon">
-                        <p>Free health classes</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-comp.png" alt="online portal icon">
-                        <p>Online patient portal and medical records</p>
-                    </div>
-
-                    <div class="small-6 medium-4 columns">
-                        <img src="../images/tvhlp-grfx-you-ico-med.png" alt="acute care icon">
-                        <p>Weekend acute care</p>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="large-12 small-centered columns">
-                <div class="row">
+            <div class="footer-msg large-12 small-centered columns">
+                <div class="row collapse">
                     <div class="small-12 columns">
-                        <h2>Learn More About<br>Medicare Advantage</h2>
-                        <p>Educate yourself about the health insurance plans we accept and the benefits of The Villages Health and Medicare Advantage. <a href="http://thevillageshealth.com/info">Learn More</a>.</p>
+                        <h2>Health care the way it was meant to be.</h2>
                     </div>
                 </div>
             </div>
@@ -254,7 +207,7 @@
                 <p>&nbsp;</p>
             </div>
         </div>
-        <div class="row">
+        <div class="row expanded">
             <div class="h4-like small-12 columns"><h4><a href="https://www.facebook.com/TheVillagesHealth" target="_blank"><img src="../images/tvhlp-grfx-social-share-fb.png" style="position:relative; top:-4px; padding-right:.25em;">Like Us On Facebook</a></h4>
             </div>
         </div>
